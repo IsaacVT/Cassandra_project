25aa1b9d7bfb25e9ed983205301bb564fd797bb8
Nallelyht <nallelyht@gmail.com>
2023-05-07 23:08:51 -0600

    fix description

--- cliente/src/pages/ProductsPage.js
+++ cliente/src/pages/ProductsPage.js
@@ -23,7 +23,7 @@ export default function ProductsPage() {
           name: product.name,
           stock: product.amount,
           price: product.price,
-          description: 'Belle oze amariyo, semidesnude acompañado de flores varias',
+          description: `Beautiful ${product.name}`,
           cover: product.cover || DefaultImg,
         }))
       );
@@ -49,7 +49,7 @@ export default function ProductsPage() {
             {edit && <ProductModalNew className={edit ? 'show-element' : null} />}
 
             <Button onClick={() => setEdit(!edit)} variant="outlined">
-              {edit ? `Cloose editor` : `Open editor`}
+              {edit ? `Close editor` : `Open editor`}
             </Button>
           </Stack>
         </Stack>
